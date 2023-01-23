aeb5de04fcb477101d6475abba6fadfacc295454
Chris Griffing <cmgriffing@gmail.com>
2023-01-23 10:58:40 -0800

    feat: add caching for users

--- apps/react-client/src/app/services/user.services.ts
+++ apps/react-client/src/app/services/user.services.ts
@@ -1,12 +1,24 @@
 import { User } from '@acme/shared-models';
+import { AxiosResponse } from 'axios';
 import { BaseService } from './base.service';
 
 export class UserService extends BaseService {
-  getUsers() {
-    return this.axios.get('/users');
+  cachedUsers: User[] = [];
+  cachedUsersTimestamp = 0;
+
+  async getUsers() {
+    if (this.cachedUsersTimestamp < Date.now() - 30_000) {
+      this.cachedUsersTimestamp = Date.now();
+      return this.axios.get('/users').then((response) => {
+        this.cachedUsers = response?.data;
+        return response;
+      });
+    } else {
+      return { data: this.cachedUsers, status: 304 } as AxiosResponse<User[]>;
+    }
   }
 
   getUser(userId: string) {
-    this.axios.get<User>(`/users/${userId}`);
+    return this.axios.get<User>(`/users/${userId}`);
   }
 }
